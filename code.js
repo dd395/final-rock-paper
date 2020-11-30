8e019ac2f8e204061eb539d6d74bc0fc2c11e81c
dd395 <drichardssmith@my.wctc.edu>
2020-11-30 15:10:18 -0600

    help needed

--- code.js
+++ code.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     //Event Listeners
 
     choices.forEach(choice => choice.addEventListener('click', play));
-    window.addEventListener('click', clearModal);
-    restart.addEventListener('click', restartGame);
+
+
 
 })
 
@@ -19,15 +19,19 @@ $(document).ready(function () {
         computer: 0
     }
 
-    //Play game
+    console.log("start play");
+
+
+
+//Play game
     function play(e) {
         restart.style.display = `inline-block`;
         const playerChoice = e.target.id;
         const computerChoice = getComputerChoice();
         const winner = getWinner(playerChoice, computerChoice);
-        //showWinner(winner, computerChoice);
+       //showWinner(winner, computerChoice);
 
-        //console.log(playerChoice, computerChoice, winner);
+       // console.log(playerChoice, computerChoice, winner);
 
         //Get computers choice
         function getComputerChoice() {
@@ -92,10 +96,67 @@ $(document).ready(function () {
                 }
 
 
-
-
-
+                function showWinner(winner, computerChoice){
+                    if(winner===`player`) {
+                        //player score
+                        scoreboard.player++;
+                        //modal result
+                        result.innerHTML =
+                            `<h1 class="text-win">You Win</h1>
+                <i class ="fas fa-hand-${computerChoice} fa-10X"></i>
+                <p>Computer chose<strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>`;
+                    }else if(winner===`computer`) {
+                        //computer score
+                        scoreboard.computer++;
+                        //modal result
+                        result.innerHTML =
+                            `<h1 class="text-win">You Lose</h1>
+                <i class ="fas fa-hand-${computerChoice} fa-10X"></i>
+                <p>Computer chose<strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>`;
+                    }else{
+                        result.innerHTML =
+                            `<h1>It's A Draw</h1>
+                <i class ="fas fa-hand-${computerChoice} fa-10X"></i>
+                <p>Computer chose<strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>`;
                     }
+                    //Show score
+                    score.innerHTML = `
+            <p>Player: ${scoreboard.player}</p>
+             <p>Computer:${scoreboard.computer}</p>
+             `;
+                    modal.style.display ="block";
+                }
+
+                //Restart game
+                function restartGame(){
+                    scoreboard.player = 0;
+                    scoreboard.computer = 0;
+                    score.innerHTML = `
+            <p>Player: 0</p>
+            <p>Computer: 0</p>
+            `;
+                }
+
+                //Clear modal
+                function clearModal(e){
+                    if(e.target===modal){
+                        modal.style.display = 'none';
+                    }
+                }
+
+
+                showWinner();
+                window.addEventListener('click', clearModal);
+                restart.addEventListener('click', restartGame);
+
+                console.log("end play");
+
+
+
+
+
+
+            }
                 }
             }
 
